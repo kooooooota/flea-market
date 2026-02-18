388d1bb21d03891c9df901201dbda42ce397396d
kota <ntgdsk@gmail.com>
2026-02-18 11:10:48 +0900

    ShippingAddressTest作成

--- src/tests/Feature/ShippingAddressTest.php
+++ src/tests/Feature/ShippingAddressTest.php
@@ -9,6 +9,7 @@ use App\Enums\Condition;
 use App\Models\User;
 use App\Models\Item;
 use App\Models\Profile;
+use App\Models\PaymentMethod;
 
 class ShippingAddressTest extends TestCase
 {
@@ -24,7 +25,7 @@ class ShippingAddressTest extends TestCase
     {
         $user = User::factory()->create();
         $seller = User::factory()->create();
-        $item = $item = Item::create([
+        $item = Item::create([
             'user_id' => $seller->id,
             'image_path' => 'items/Armani+Mens+Clock.jpg',
             'name' => '腕時計',
@@ -33,7 +34,7 @@ class ShippingAddressTest extends TestCase
             'description' => 'スタイリッシュなデザインのメンズ腕時計',
             'category_ids' => [1, 5],
             'condition' => Condition::LikeNew->value,
-            'sold' => true,
+            'sold' => false,
         ]);
 
         Profile::create([
@@ -43,23 +44,48 @@ class ShippingAddressTest extends TestCase
             'zip_code' => '111-1111',
             'address' => '渋谷区千駄ヶ谷',
         ]);
-
-        $this->actingAs($user)
-             ->get(route('items.to_address_form', $item))
-             ->assertStatus(200);
-
+        
         $formData = [
             'zip_code' => '222-2222',
             'address' => '渋谷区渋谷',
             'building' => '渋谷マンション101',
-        ];
+            ];
 
-        $response = $this->post(route('items.change_address', $formData));
+        $response = $this->actingAs($user)
+                ->from(route('items.to_address_form', $item))
+                ->post(route('items.change_address', $item), $formData);
+            
+        $response->assertRedirect(route('items.checkout', $item));
 
-        $response->assertRedirect('items.checkout', $item);
+        $finalResponse = $this->get(route('items.checkout', $item));
 
-        $response->assertSee($formData->zip_code)
-                 ->assertSee($formData->address)
-                 ->assertSee($formData->building);
+        $finalResponse->assertStatus(200)
+                      ->assertSee($formData['zip_code'])
+                      ->assertSee($formData['address'])
+                      ->assertSee($formData['building']);
+
+        $paymentMethod = PaymentMethod::create([
+            'method' => 'コンビニ払い'
+        ]);
+        
+        $response = $this->actingAs($user)
+                         ->post(route('purchase.checkout', $item), [
+                            'payment_method' => $paymentMethod->id,
+                         ]);
+
+        if ($response->isRedirect()) {
+            $response = $this->followRedirects($response);
+        }
+        
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('purchased_items', [
+            'user_id' => $user->id,
+            'item_id' => $item->id,
+            'zip_code' => $formData['zip_code'],
+            'address' => $formData['address'],
+            'building' => $formData['building'],
+            'payment_method_id' => $paymentMethod->id,
+        ]);
     }
 }
